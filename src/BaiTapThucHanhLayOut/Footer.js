7cac3784f897567bcd13f0fce36395f3e0e9005d
Liên Hồ <holien227305@gmail.com>
2023-11-25 19:42:00 +0700

    update baitap

---
+++ src/BaiTapThucHanhLayOut/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bg-dark">
+          <p className='text-white text-center py-5'>Copyright © Your Website 2023</p>
+        </div>
+      </div>
+    )
+  }
+}
